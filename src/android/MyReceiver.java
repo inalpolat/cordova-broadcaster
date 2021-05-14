ce993427d65011f9bf3b81e17f055129a13680bc
inalpolat <inalpolat@yahoo.com>
2021-05-15 00:19:44 +0300

    Update MyReceiver.java

--- src/android/MyReceiver.java
+++ src/android/MyReceiver.java
@@ -8,6 +8,22 @@ import android.support.v4.content.LocalBroadcastManager;
 public class MyReceiver extends BroadcastReceiver {	
 	@Override
 	public void onReceive(Context context, Intent intent) {
-                 LocalBroadcastManager.getInstance(context).sendBroadcastSync(intent);
+                 //LocalBroadcastManager.getInstance(context).sendBroadcastSync(intent);
+
+		final Intent intent = new Intent("didShow");
+
+		final Bundle child = new Bundle();
+		child.putString( "name", "joker");
+
+		final Bundle b = new Bundle();
+		b.putString( "data", "test");
+		b.putBoolean( "valid", true );
+		b.putBundle( "child", child );
+
+		intent.putExtras( b);
+
+		LocalBroadcastManager.getInstance(this).sendBroadcastSync(intent);		
+		
+		
 	}
 }
