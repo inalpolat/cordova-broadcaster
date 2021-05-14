0368e3eb0760988158ef0fd0a1dffe5b7bab41ba
inalpolat <inalpolat@yahoo.com>
2021-05-14 23:21:18 +0300

    Update MyReceiver.java

--- src/android/MyReceiver.java
+++ src/android/MyReceiver.java
@@ -1,25 +1,6 @@
-package org.bsc.cordova;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Bundle;
-import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
-
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Iterator;
-
-import static java.lang.String.format;
-import static java.lang.String.valueOf;
-
 
 public class MyReceiver extends BroadcastReceiver {	
 	@Override
