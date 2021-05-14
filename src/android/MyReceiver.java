3dcb168a948e5ac000d18fca45499d2da00b719b
inalpolat <inalpolat@yahoo.com>
2021-05-15 01:59:26 +0300

    Update MyReceiver.java

--- src/android/MyReceiver.java
+++ src/android/MyReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.support.v4.content.LocalBroadcastManager;
 
 public class MyReceiver extends BroadcastReceiver {	
