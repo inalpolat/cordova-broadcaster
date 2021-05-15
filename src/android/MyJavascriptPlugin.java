1d391c14d0f4d8a63ba6588cf3f95120cc720f56
inalpolat <inalpolat@yahoo.com>
2021-05-15 21:05:40 +0300

    Update MyJavascriptPlugin.java

--- src/android/MyJavascriptPlugin.java
+++ src/android/MyJavascriptPlugin.java
@@ -2,6 +2,7 @@ package org.bsc.cordova;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
 import android.util.Log;
 
 public class MyJavascriptPlugin extends CordovaPlugin {
