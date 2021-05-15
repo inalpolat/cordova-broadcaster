411649a7a12348230f34ad7c97c14068dabcbfdf
inalpolat <inalpolat@yahoo.com>
2021-05-15 19:21:19 +0300

    Update MyJavascriptPlugin.java

--- src/android/MyJavascriptPlugin.java
+++ src/android/MyJavascriptPlugin.java
@@ -5,7 +5,8 @@ import org.apache.cordova.CordovaPlugin;
 public class MyJavascriptPlugin extends CordovaPlugin {
   
   void fireEvent(){
-  
+    Log.e("myTag", "*******************BH999*****************");
+    Log.e("myTag", "*******************BH8888*****************");
   }
   
 }
