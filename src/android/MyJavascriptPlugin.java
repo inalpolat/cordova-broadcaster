4c9e3bc4a0cc500ddeec3ca1617e5da7f3c8b4e2
inalpolat <inalpolat@yahoo.com>
2021-05-15 20:45:43 +0300

    Update MyJavascriptPlugin.java

--- src/android/MyJavascriptPlugin.java
+++ src/android/MyJavascriptPlugin.java
@@ -12,14 +12,23 @@ public class MyJavascriptPlugin extends CordovaPlugin {
     String method = "javascript:window.broadcaster.fireEvent( 'baris', '123' );";
     
     //webView.loadUrl(method);
-    cordova.getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Log.e("myTag", "*******************BH300*****************");              
-                webView.loadUrl(method);
-                Log.e("myTag", "*******************BH400*****************");                
-            }
-        });    
-  }
+//    cordova.getActivity().runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.e("myTag", "*******************BH300*****************");              
+//                webView.loadUrl(method);
+//                Log.e("myTag", "*******************BH400*****************");                
+//            }
+//        });    
+//  }
+  
+  
+      cordova.getThreadPool().execute(new Runnable() {
+                  public void run() {
+                  Log.e("myTag", "*******************BH300*****************");              
+                  webView.loadUrl(method);
+                  Log.e("myTag", "*******************BH400*****************");                
+                }
+            });  
   
 }
